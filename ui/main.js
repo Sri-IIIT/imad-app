b68b634b8ae78e27cc561ccbbb12b463458f928c
Sri-IIIT <sri.mycareer@gmail.com>
2017-08-24 22:11:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button = document.getElementsById('counter');
+var count=0;
+button.onclick = function(){
+      counter = counter + 1;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();
+};
